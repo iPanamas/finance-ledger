1cf5ce2168cff5007bea4074ee512c2d10a95ca9
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-07-29 11:59:20 +0300

    clone project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './sass/main.scss';
+import API from './scripts/services/API';
+import renderMainPage from './scripts/renderMainPage';
+import { scrollToTop } from './scripts/scroll-to-top';
+import header from './scripts/header';
+import pagination from './scripts/pagination';
+import spinner from './scripts/spinner';
+// import './scripts/searchMovie';
+import modalStudents from './scripts/modalStudents';
+import darkTheme from './scripts/dark-theme';
+import sortingBtns from './scripts/sortingBtns';
+import './scripts/infiniteScroll';
