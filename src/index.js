f8fa7b6531f621315ba361d323bdb7a07d00d3a4
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-08-10 10:54:58 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+
+const headerEl = document.querySelector('.header');
+
+window.onscroll = () => {
+  if (window.matchMedia('(min-width: 1360px)').matches) {
+    window.scrollY > 70
+      ? (headerEl.style.backgroundColor = '#000000cc')
+      : (headerEl.style.backgroundColor = 'transparent');
+  }
+
+  if (window.matchMedia('(max-width: 768px)').matches) {
+    window.scrollY > 120
+      ? (headerEl.style.backgroundColor = '#000000cc')
+      : (headerEl.style.backgroundColor = 'transparent');
+  }
+};
+
+console.log(scrollY);
