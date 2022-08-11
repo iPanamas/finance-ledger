d3b2e8b68a91570e54de6a1125a1c4a0d0a07d2c
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-08-11 13:46:53 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import './sass/main.scss';
 import { headerOpacity } from './js/headerOpacity';
 import { validationForm } from './js/validationForm';
 
+const wrapper = document.querySelector('.team-images__wrapper');
+const overlay = document.querySelector('.team-overlay');
+
+const showSocial = () => {
+  overlay.classList.remove('visually-hidden');
+};
+wrapper.addEventListener('mouseover', showSocial);
+
+const hideSocial = () => {
+  overlay.classList.add('visually-hidden');
+};
+wrapper.addEventListener('mouseout', hideSocial);
+
 headerOpacity();
 validationForm();
