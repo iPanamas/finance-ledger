12724868b4b7a0e07cc7d325709187db073e41fd
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-08-11 23:11:41 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import './sass/main.scss';
 
-
 import { headerOpacity } from './js/headerOpacity';
 import { validationForm } from './js/validationForm';
-import { lightbox } from './js/gallery';
 
 headerOpacity();
 validationForm();
-lightbox();
