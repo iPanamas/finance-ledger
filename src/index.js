34684e0c12363e822c84e927ed238794811f5fc9
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-08-11 18:07:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import './sass/main.scss';
+
+
 import { headerOpacity } from './js/headerOpacity';
 import { validationForm } from './js/validationForm';
-
-const wrapper = document.querySelector('.team-images__wrapper');
-const overlay = document.querySelector('.team-overlay');
-
-const showSocial = () => {
-  overlay.classList.remove('visually-hidden');
-};
-wrapper.addEventListener('mouseover', showSocial);
-
-const hideSocial = () => {
-  overlay.classList.add('visually-hidden');
-};
-wrapper.addEventListener('mouseout', hideSocial);
+import { lightbox } from './js/gallery';
 
 headerOpacity();
 validationForm();
+lightbox();
---
+++ src/js/gallery.js
@@ -0,0 +1,17 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.cases-images__item');
+
+export const lightbox = () => {
+  return (lightbox = new SimpleLightbox('.cases-images__item a', {
+    showCounter: false,
+    scaleImageToRatio: true,
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 100,
+    disableRightClick: true,
+    scrollZoom: false,
+    overlayOpacity: 0.7,
+  }));
+};
