28485cdafafd3843e7c20822f3ea907ece6afe80
iPanamas <88848737+iPanamas@users.noreply.github.com>
2022-08-13 07:45:11 +0000

    Deploying to gh-pages from @ iPanamas/finance-ledger@039d65b2f80825d303b05ac3a11539f40ac11a8d 🚀

--- src.27a0babf.js
+++ src.27a0babf.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");var r=require("./js/headerOpacity"),a=require("./js/validationForm");(0,r.headerOpacity)(),(0,a.validationForm)();
 },{"./sass/main.scss":"clu1","./js/headerOpacity":"iwE8","./js/validationForm":"A7Dh"}]},{},["Focm"], null)
-//# sourceMappingURL=/finance-ledger/src.866b1169.js.map
+//# sourceMappingURL=/src.27a0babf.js.map
